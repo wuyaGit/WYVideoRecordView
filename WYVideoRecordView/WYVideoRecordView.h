6bc577f225fd14336b52bce34d29af018bd9beb0
Yangguangliang <xwu@YangguangliangdeMacBook-Air.local>
2018-01-04 21:06:52 +0800

    添加了 .podspec文件

---
+++ WYVideoRecordView/WYVideoRecordView.h
@@ -0,0 +1,13 @@
+//
+//  WYVideoRecordView.h
+//  WYVideoRecordViewDemo
+//
+//  Created by Yangguangliang on 2018/1/4.
+//  Copyright © 2018年 Yangguangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYVideoRecordView : UIView
+
+@end
---
+++ WYVideoRecordView/WYVideoRecordView.m
@@ -0,0 +1,21 @@
+//
+//  WYVideoRecordView.m
+//  WYVideoRecordViewDemo
+//
+//  Created by Yangguangliang on 2018/1/4.
+//  Copyright © 2018年 Yangguangliang. All rights reserved.
+//
+
+#import "WYVideoRecordView.h"
+
+@implementation WYVideoRecordView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
